0b6725526425227be7ceed84f768f9f1c9fb5da5
portal martin <ptl_martin@yahoo.co.jp>
2024-03-18 19:42:38 +0900

    modified:   app/controllers/sessions_controller.rb 	modified:   client/src/App.jsx

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,15 @@
 class SessionsController < ApplicationController
 
-    def create
-        @user = User.find_by(email: params[:email])
-        if @user and @user.authenticate(params[:password])
-            logged_user = JWT.encode({user: @user.id}, ENV['JWT_TOKEN'])
-            render json: {uid: logged_user}, status: :ok #200
-        else
-            render json: {errors: ['Invalid credential !']}, status: :unauthorized #401
-        end
-    end
+  def create
+      @user = User.find_by(email: params[:email])
+      if @user and @user.authenticate(params[:password])
+          logged_user = JWT.encode({user: @user.id}, ENV['JWT_TOKEN'])
+          user = {id: @user.id, uid: logged_user} #new line
+          render json: user, status: :ok #200 #modified line
+      else
+          render json: {errors: ['Invalid email address and/or password!']}, status: :unauthorized #401
+      end
+  end
 
+  #end
 end
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import AppRoutes from "./component/AppRoutes";
 import NavBar from "./component/NavBar/NavBar";
@@ -6,26 +6,39 @@ import NavBar from "./component/NavBar/NavBar";
 import "./App.css";
 
 function App() {
+  const [currentUser, setCurrentUser] = useState(null);
+
   useEffect(() => {
     if (localStorage.uid) console.log("User found:", localStorage.uid);
     else console.log("User not found");
   }, []);
 
   async function checkUser() {
-    fetch(`http://localhost:3000/login`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json",
-      },
-      body: JSON.stringify({
-        email: "satoshi@nakamoto.com",
-        password: "Satoshi123!",
-      }),
-    })
-      .then((resp) => resp.json())
-      .then((user) => (localStorage.uid = user.uid));
+    try {
+      const response = await fetch(`http://localhost:3000/login`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify({
+          email: "satoshi@nakamoto.com",
+          password: "Satoshi123!",
+        }),
+      });
+
+      if (!response.ok) {
+        throw new Error("Failed to login");
+      }
+
+      const user = await response.json();
+      localStorage.setItem("uid", user.uid); // Utilisation de setItem pour stocker la valeur dans le localStorage
+      setCurrentUser(user.id); // Assurez-vous que setCurrentUser fonctionne correctement
+    } catch (error) {
+      console.error("Error:", error);
+    }
   }
+
   checkUser();
   return (
     <>

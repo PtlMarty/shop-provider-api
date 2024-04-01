bf6a764a589e25f1c5878672668ef418abbacd3d
portal martin <ptl_martin@yahoo.co.jp>
2024-04-01 20:39:55 +0900

    correct bug delete shop &&//or product

--- client/src/component/pages/Home.jsx
+++ client/src/component/pages/Home.jsx
@@ -1,7 +1,23 @@
 import React from "react";
 
 function Home() {
-  return <div></div>;
+  return (
+    <div className="d-flex flex-column">
+      <div>
+        <h1>Home</h1>
+        <p>Welcome to the Home page</p>
+      </div>
+      <div className="w-75 m-auto">
+        <h2>What is this?</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam
+          nesciunt, repellat accusantium aliquam error sunt debitis omnis, eos
+          nobis, laudantium odit? Necessitatibus, quidem quia non animi
+          consectetur itaque facere quos.
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
